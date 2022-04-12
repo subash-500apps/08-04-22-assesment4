52555f11543495afd8afb7f04306be23baca8bbb
subash-500apps <102018284+subash-500apps@users.noreply.github.com>
2022-04-12 17:25:41 +0530

    Add files via upload

---
+++ promise.js
@@ -0,0 +1,33 @@
+
+
+
+const cheerio = require('cheerio');
+const rp = require('request-promise');
+
+const url = 'https://www.agilecrm.com/';
+// I use Wikipedia just for testing purpose
+
+rp(url).then(html => {
+  const $ = cheerio.load(html);
+    const linkObjects = $('a');
+    // this is a mass object, not an array
+
+    const total = linkObjects.length;
+    // The linkObjects has a property named "lenght"
+
+    const links = [];
+    // we only need the "href" and "title" of each link
+
+    for(let i = 0; i < total; i++){
+        links.push({
+            href: linkObjects[i].attribs.href,
+            title: linkObjects[i].attribs.name
+        });
+    }
+
+    console.log(links);
+    
+})
+.catch(err => {
+    console.log(err); 
+})
